90bc99ba030952637d45dd51553e0fe1238d7cc9
Agnaldo <agnaldo.pereira@corebiz.ag>
2021-11-05 10:12:58 -0300

    adicionado componente de Countdown e Title

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -6,3 +6,11 @@ declare global {
     getSchema?(props?: P): object
   }
 }
+
+export interface TimeSplit {
+  hours: string
+  minutes: string
+  seconds: string
+}
+
+type GenericObject = Record<string, any>
